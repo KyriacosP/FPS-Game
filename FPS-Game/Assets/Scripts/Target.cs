10543a5b2a76dd78cf0be05abc3629d85fbf75dd
KyriacosP <44581807+KyriacosP@users.noreply.github.com>
2021-03-05 23:46:20 +0200

    Shooting implementation

---
+++ FPS-Game/Assets/Scripts/Target.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float health = 50f;
+
+    public void Damage(float damage)
+    {
+        health -= damage;
+        if(health <= 0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
